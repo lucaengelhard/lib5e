f4c81ab7f049fbc2c99a16e729f7d3cd3c90e0b4
Luca Engelhard <me@lucaengelhard.com>
2026-09-13 19:29:16 +0200

    sugar

---
+++ src/components.ts
@@ -0,0 +1,225 @@
+import {
+  createDesugarer,
+  desugar,
+  type Extend,
+  type Node,
+  type NodeFactory,
+} from "@lucaengelhard/libttrpg";
+
+export const GLOBAL_VALUES = {
+  LEVEL: {
+    type: "VALUE",
+    name: "stats.level",
+    value: { type: "VALUE", value: 0 },
+  },
+  PROFICIENCY_BONUS: {
+    type: "VALUE",
+    name: "stats.proficiencyBonus",
+    value: {
+      type: "VALUE",
+      value: {
+        type: "BINARYOPERATION",
+        kind: "ADD",
+        left: { type: "VALUE", value: 1 },
+        right: {
+          type: "UNARYOPERATION",
+          kind: "CEIL",
+          value: {
+            type: "BINARYOPERATION",
+            kind: "MULTIPLY",
+            left: { type: "VALUE", value: 1 / 4 },
+            right: { type: "QUERY", query: "stats.level" },
+          },
+        },
+      },
+    },
+  },
+  WALKING_SPEED: { type: "VALUE", name: "stats.speed.walking", value: 0 },
+  CLIMBING_SPEED: { type: "VALUE", name: "stats.speed.climbing", value: 0 },
+  SWIMMING_SPEED: { type: "VALUE", name: "stats.speed.swimming", value: 0 },
+  FLYING_SPEED: { type: "VALUE", name: "stats.speed.flying", value: 0 },
+} as const satisfies Record<string, Node>;
+
+export const GLOBAL_VALUE_NAMES = Object
+  .fromEntries(
+    Object.entries(GLOBAL_VALUES).map((
+      [key, value],
+    ) => [key, value.name] as const),
+  ) as Readonly<Record<keyof typeof GLOBAL_VALUES, string>>;
+
+type Ability = NodeFactory<"Ability", { name: string; base: number }>;
+function ABILITY(input: Ability): Node {
+  return {
+    type: "SECTION",
+    name: `__abilities__${input.name}`,
+    value: {
+      type: "MULTIPLE",
+      values: [
+        { type: "VALUE", name: `abilities.${input.name}`, value: input.base },
+        {
+          type: "VALUE",
+          name: `modifiers.${input.name}`,
+          value: {
+            type: "UNARYOPERATION",
+            kind: "FLOOR",
+            value: {
+              type: "BINARYOPERATION",
+              kind: "DIVIDE",
+              left: {
+                type: "BINARYOPERATION",
+                kind: "SUBTRACT",
+                left: { type: "QUERY", query: `abilities.${input.name}` },
+                right: { type: "VALUE", value: 10 },
+              },
+              right: { type: "VALUE", value: 2 },
+            },
+          },
+        },
+        {
+          type: "VALUE",
+          name: `saves.${input.name}`,
+          value: { type: "QUERY", query: `modifiers.${input.name}` },
+        },
+        {
+          type: "MODIFIER",
+          value: {
+            type: "BINARYOPERATION",
+            kind: "MULTIPLY",
+            left: {
+              type: "BINARYOPERATION",
+              kind: "MAX",
+              left: { type: "VALUE", value: 0 },
+              right: {
+                type: "AGGREGATOR",
+                kind: "MAX",
+                name: `proficiencies.saves.${input.name}`,
+              },
+            },
+            right: { type: "QUERY", query: GLOBAL_VALUE_NAMES.LEVEL },
+          },
+          target: `saves.${input.name}`,
+        },
+      ],
+    },
+  };
+}
+
+type Skill = NodeFactory<
+  "Skill",
+  { name: string; ability: string; hasPassive?: boolean }
+>;
+function SKILL(input: Skill): Node {
+  const passive: Node[] = input.hasPassive
+    ? [{
+      type: "VALUE",
+      name: `passives.${input.name}`,
+      value: {
+        type: "BINARYOPERATION",
+        kind: "ADD",
+        left: { type: "VALUE", value: 10 },
+        right: { type: "QUERY", query: `skills.${input.name}` },
+      },
+    }]
+    : [];
+
+  return {
+    type: "SECTION",
+    name: `__skills__${input.name}`,
+    value: {
+      type: "MULTIPLE",
+      values: [
+        {
+          type: "VALUE",
+          name: `skills.${input.name}`,
+          value: { type: "QUERY", query: `modifiers.${input.ability}` },
+        },
+        {
+          type: "MODIFIER",
+          value: {
+            type: "BINARYOPERATION",
+            kind: "MULTIPLY",
+            left: {
+              type: "AGGREGATOR",
+              kind: "MAX",
+              name: `proficiencies.skills.${input.name}`,
+            },
+            right: {
+              type: "QUERY",
+              query: GLOBAL_VALUE_NAMES.PROFICIENCY_BONUS,
+            },
+          },
+          target: `skills.${input.name}`,
+        },
+        ...passive,
+      ],
+    },
+  };
+}
+
+type ProficiencyValue = 0.5 | 1 | 2;
+type Proficiency = NodeFactory<"Proficiency", {
+  target: string;
+  value: ProficiencyValue;
+}>;
+function PROFICIENCY(input: Proficiency): Node {
+  return {
+    type: "MODIFIER",
+    value: { type: "VALUE", value: input.value },
+    target: `proficiencies.${input.target}`,
+  };
+}
+
+type Class = NodeFactory<
+  "Class",
+  { name: string; value: Node | Component; level: number }
+>;
+function CLASS(input: Class): Node {
+  return {
+    type: "SECTION",
+    name: `__classes__${input.name}`,
+    value: {
+      type: "MULTIPLE",
+      values: [
+        {
+          type: "MODIFIER",
+          target: GLOBAL_VALUE_NAMES.LEVEL,
+          value: {
+            type: "VALUE",
+            name: `classes.${input.name}.level`,
+            value: input.level,
+          },
+        },
+        input.value as Node,
+      ],
+    },
+  };
+}
+
+type Species = NodeFactory<
+  "Species",
+  { name: string; value: Node | Component }
+>;
+function SPECIES(input: Species): Node {
+  return {
+    type: "SECTION",
+    name: `__species__${input.name}`,
+    value: input.value as Node,
+  };
+}
+
+export type Component = Ability | Skill | Proficiency | Class | Species;
+
+export const componentDesugar = createDesugarer<
+  Component,
+  Extend<Node, Component, Node>,
+  Node
+>(
+  {
+    PROFICIENCY: (node, internalDesugar) => internalDesugar(PROFICIENCY(node)),
+    ABILITY: (node, internalDesugar) => internalDesugar(ABILITY(node)),
+    SKILL: (node, internalDesugar) => internalDesugar(SKILL(node)),
+    CLASS: (node, internalDesugar) => internalDesugar(CLASS(node)),
+    SPECIES: (node, internalDesugar) => internalDesugar(SPECIES(node)),
+  },
+  desugar,
+);
--- src/mod.ts
+++ src/mod.ts
@@ -1,1 +1,120 @@
-import { desugar, parse } from "@lucaengelhard/libttrpg";
+/* console.log(
+  parse(
+    desugar({
+      type: "MULTIPLE",
+      values: [
+        GLOBAL_VALUES.LEVEL,
+        GLOBAL_VALUES.PROFICIENCY_BONUS,
+        GLOBAL_VALUES.WALKING_SPEED,
+        GLOBAL_VALUES.SWIMMING_SPEED,
+        GLOBAL_VALUES.CLIMBING_SPEED,
+        GLOBAL_VALUES.FLYING_SPEED,
+        ABILITY({
+          type: "ABILITY",
+          name: "wisdom",
+          base: 12,
+        }),
+        ABILITY({
+          type: "ABILITY",
+          name: "charisma",
+          base: 8,
+        }),
+        SKILL({
+          type: "SKILL",
+          name: "perception",
+          ability: "wisdom",
+          hasPassive: true,
+        }),
+        CLASS({
+          type: "CLASS",
+          name: "ranger",
+          level: 4,
+          value: { type: "MULTIPLE", values: [] },
+        }),
+        SPECIES({
+          type: "SPECIES",
+          name: "halfElf",
+          value: {
+            type: "MULTIPLE",
+            values: [
+              {
+                type: "MODIFIER",
+                target: "stats.speed.walking",
+                value: { type: "VALUE", value: 30 },
+              },
+              {
+                type: "MODIFIER",
+                target: "abilities.charisma",
+                value: { type: "VALUE", value: 2 },
+              },
+            ],
+          },
+        }),
+      ],
+    }),
+  ),
+); */
+
+import { componentDesugar } from "./components.ts";
+
+console.log(
+  componentDesugar({
+    type: "CLASS",
+    name: "Ranger",
+    level: 4,
+    value: { type: "MULTIPLE", values: [] },
+  }),
+);
+
+/* console.log(desugar({
+  type: "MULTIPLE",
+  values: [
+    GLOBAL_VALUES.LEVEL,
+    GLOBAL_VALUES.PROFICIENCY_BONUS,
+    GLOBAL_VALUES.WALKING_SPEED,
+    GLOBAL_VALUES.SWIMMING_SPEED,
+    GLOBAL_VALUES.CLIMBING_SPEED,
+    GLOBAL_VALUES.FLYING_SPEED,
+    ABILITY({
+      type: "ABILITY",
+      name: "wisdom",
+      base: 12,
+    }),
+    ABILITY({
+      type: "ABILITY",
+      name: "charisma",
+      base: 8,
+    }),
+    SKILL({
+      type: "SKILL",
+      name: "perception",
+      ability: "wisdom",
+      hasPassive: true,
+    }),
+    CLASS({
+      type: "CLASS",
+      name: "ranger",
+      level: 4,
+      value: { type: "MULTIPLE", values: [] },
+    }),
+    SPECIES({
+      type: "SPECIES",
+      name: "halfElf",
+      value: {
+        type: "MULTIPLE",
+        values: [
+          {
+            type: "MODIFIER",
+            target: "stats.speed.walking",
+            value: { type: "VALUE", value: 30 },
+          },
+          {
+            type: "MODIFIER",
+            target: "abilities.charisma",
+            value: { type: "VALUE", value: 2 },
+          },
+        ],
+      },
+    }),
+  ],
+})); */
