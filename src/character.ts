1bf88753ef5bfa961d6bc7b1b368c9ca0fbfcec8
Luca Engelhard <me@lucaengelhard.com>
2026-09-18 01:40:54 +0200

    character fixes

--- src/character.ts
+++ src/character.ts
@@ -1,21 +1,25 @@
 import {
   deleteNode,
+  type Filter,
   getValue,
   hasValue,
   parse,
   ResolverMap,
   setValue,
+  type Statement,
 } from "@lucaengelhard/libttrpg";
 import {
-  type ComponentTree,
+  ComponentFactory,
   desugarComponent,
   GLOBAL_VALUES,
+  type Nodes,
 } from "./components.ts";
 
-type CharacterTree = Extract<ComponentTree, { $type: "MULTIPLE" }>;
+const { MULTIPLE, ABILITY, CLASS, SKILL, SECTION_STATEMENT } = ComponentFactory;
+
+type CharacterTree = Extract<Nodes, { $type: "MULTIPLE" }>;
 export class Character {
-  #tree: CharacterTree = {
-    $type: "MULTIPLE",
+  #tree: CharacterTree = MULTIPLE({
     values: [
       GLOBAL_VALUES.LEVEL,
       GLOBAL_VALUES.PROFICIENCY_BONUS,
@@ -24,18 +28,18 @@ export class Character {
       GLOBAL_VALUES.CLIMBING_SPEED,
       GLOBAL_VALUES.FLYING_SPEED,
     ],
-  };
+  });
 
-  constructor(tree?: Extract<ComponentTree, { $type: "MULTIPLE" }>) {
+  constructor(tree?: CharacterTree) {
     if (tree) this.#tree = tree;
   }
 
   getTree() {
-    return structuredClone(this.#tree as ComponentTree);
+    return this.#tree;
   }
 
   desugar() {
-    return desugarComponent(this.#tree as ComponentTree);
+    return desugarComponent(this.#tree as Nodes);
   }
 
   resolve() {
@@ -43,28 +47,28 @@ export class Character {
   }
 
   get<
-    Type extends Extract<ComponentTree, { name?: string }>["$type"],
-    Key extends keyof Extract<ComponentTree, { $type: Type }>,
+    Type extends Extract<Nodes, { name?: string }>["$type"],
+    Key extends keyof Extract<Nodes, { $type: Type }>,
   >(type: Type, name: string, key: Key) {
-    return getValue(this.#tree as ComponentTree, type, name, key);
+    return getValue(this.#tree as Nodes, type, name, key);
   }
 
   has<
-    Type extends Extract<ComponentTree, { name?: string }>["$type"],
+    Type extends Extract<Nodes, { name?: string }>["$type"],
   >(type: Type, name: string) {
-    return hasValue(this.#tree as ComponentTree, type, name);
+    return hasValue(this.#tree as Nodes, type, name);
   }
 
   set<
-    Type extends Extract<ComponentTree, { name?: string }>["$type"],
+    Type extends Extract<Nodes, { name?: string }>["$type"],
     Key extends Exclude<
-      keyof Extract<ComponentTree, { $type: Type }>,
+      keyof Extract<Nodes, { $type: Type }>,
       "$type" | "name"
     >,
-    Value extends Extract<ComponentTree, { $type: Type }>[Key],
+    Value extends Extract<Nodes, { $type: Type }>[Key],
   >(type: Type, name: string, key: Key, value: Value) {
     this.#tree = setValue(
-      this.#tree as ComponentTree,
+      this.#tree as Nodes,
       type,
       name,
       key,
@@ -74,17 +78,17 @@ export class Character {
     return this;
   }
 
-  add(node: Extract<ComponentTree, { $type: "MULTIPLE" }>["values"][number]) {
+  add(node: Statement) {
     this.#tree.values.push(node);
     return this;
   }
 
   delete(
-    type: Extract<ComponentTree, { name?: string }>["$type"],
+    type: Extract<Nodes, { name?: string }>["$type"],
     name: string,
   ) {
     this.#tree = deleteNode(
-      this.#tree as ComponentTree,
+      this.#tree as Nodes,
       type,
       name,
     ) as CharacterTree;
@@ -95,22 +99,22 @@ export class Character {
   setAbilityBase(name: string, base: number) {
     return this.has("ABILITY", name)
       ? this.set("ABILITY", name, "base", base)
-      : this.add({ $type: "ABILITY", name, base });
+      : this.add(ABILITY({ name, base }));
   }
 
   addSkill(name: string, ability: string, hasPassive?: boolean) {
     return this.has("SKILL", name)
       ? this
-      : this.add({ $type: "SKILL", name, ability, hasPassive });
+      : this.add(SKILL({ name, ability, hasPassive }));
   }
 
   addClass(
     name: string,
-    value: Extract<ComponentTree, { $type: "CLASS" }>["value"],
+    value: Filter<Nodes, "CLASS">["value"],
   ) {
     return this.has("CLASS", name)
       ? this
-      : this.add({ $type: "CLASS", name, value, level: 1 });
+      : this.add(CLASS({ name, value, level: 1 }));
   }
 
   setClassLevel(name: string, level: number) {
@@ -136,11 +140,9 @@ export class Character {
     );
   }
 
-  setSpecies(
-    value: Extract<ComponentTree, { $type: "MULTIPLE" }>["values"][number],
-  ) {
+  setSpecies(value: Statement) {
     return this.has("SECTION", "__SPECIES__")
       ? this.set("SECTION", "__SPECIES__", "value", value)
-      : this.add({ $type: "SECTION", name: "__SPECIES__", value });
+      : this.add(SECTION_STATEMENT({ name: "__SPECIES__", value }));
   }
 }
